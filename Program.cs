c768d5d8bfe808904115c952794d9f3ddfb007db
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-07-14 13:52:29 -0400

    getting started on chapter 5 exercises

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chapter5exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
